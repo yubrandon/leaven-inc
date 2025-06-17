bb861b5de73a3f0ce023cd1f31603831304cdd46
Brandon Yu <yubrandon@debian.debian.net>
2025-06-17 15:18:39 -0700

    fixed accordion bug with bootstrap

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './css/custom.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
